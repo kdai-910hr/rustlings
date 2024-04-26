7b0b830f1f43a5ea43cf459f5059ee520affc8ce
kdai-910hr <zyf79513@163.com>
2024-04-26 17:06:05 +0800

    04261706

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(input: i32) -> i32 {
+    match input > 40 {
+        true => input,
+        false => 2 * input
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
